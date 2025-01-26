37437e48a46032a1babe05127c92a168fd936d4c
naysonpedro25 <naysonpedro2020@gmail.com>
2025-01-26 19:37:34 -0300

    Fix: New Host config

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from './app';
 import { env } from './env';
 // dividi para poder usar o app apenas no tests sem precisar rodar o server
-app.listen({ port: env.PORT }).then((addrs) => {
-  console.log(`Http server runing in ${addrs}`);
+app.listen({ port: env.PORT, host: '0.0.0.0' }).then((addrs) => {
+    console.log(`Http server runing in ${addrs}`);
 });
