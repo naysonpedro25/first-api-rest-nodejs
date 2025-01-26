5aaaa5e023c1584cfdb1740b3b9d51043808f398
naysonpedro25 <naysonpedro2020@gmail.com>
2025-01-26 04:16:03 -0300

    task: Production config

--- src/database.ts
+++ src/database.ts
@@ -4,15 +4,18 @@ import { knex as setupKnex, Knex } from 'knex';
 if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL not found');
 
 export const config: Knex.Config = {
-  client: 'sqlite',
-  connection: {
-    filename: env.DATABASE_URL,
-  },
-  useNullAsDefault: true,
-  migrations: {
-    extension: 'ts',
-    directory: './db/migrations',
-  },
+    client: env.DATABASE_CLIENT,
+    connection:
+        env.DATABASE_CLIENT === 'sqlite' // isso por conta da url no knex
+            ? {
+                  filename: env.DATABASE_URL,
+              }
+            : env.DATABASE_URL,
+    useNullAsDefault: true,
+    migrations: {
+        extension: 'ts',
+        directory: './db/migrations',
+    },
 };
 
 export const knex = setupKnex(config);
--- src/env/index.ts
+++ src/env/index.ts
@@ -16,8 +16,8 @@ const envSchema = z.object({
         .enum(['development', 'test', 'production'])
         .default('production'),
     DATABASE_URL: z.string(),
-
-    PORT: z.number().default(3000),
+    DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
+    PORT: z.coerce.number().default(3000),
 });
 
 const _env = envSchema.safeParse(process.env);
